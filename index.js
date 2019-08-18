2aa4b2cc13da1b761899980c1d3c66696c807057
Ken Huang <Ken@Kens-MacBook.local>
2019-08-17 23:27:55 -0700

    included post and get request to servver side code

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000   // process env is good for deployment setup such as using with heroku and aws
 const path = require('path')
-
+const bodyParser = require('body-parser')
 app.use(express.static('static'))
+    app.use(bodyParser.urlencoded({extended:false}))
 
-app.get('/root',(req,res)=>
+app.get('/',(req,res)=>
     res.sendFile(path.join(__dirname, 'static/index.html'))
 )
+
+app.post('/tweet', (req,res)=>{
+    console.log(req.body.tweet)
+    res.redirect('/')
+})
+
 
 app.listen(port, ()=>
     console.log(`listening on port ${port}`)
